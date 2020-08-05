0d55ed8647d8f7d125bf26c7ae3b389dc5f4aaa6
breath-of-air <molok0@protonmail.com>
2020-08-05 15:36:41 +0630

    Some changes

---
+++ Game-of-Life/Game-of-Life/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Game_of_Life
+{
+    public partial class Form1 : Form
+    {
+
+        private Graphics graphics;
+        private int resolution;
+        private bool[,] field;
+        private int rows;
+        private int cols;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame()
+        {
+            if (timer1.Enabled)
+                return;
+
+            nudResolution.Enabled = false;
+            nudDensity.Enabled = false;
+            resolution = (int)nudResolution.Value;
+            rows = pictureBox1.Height / resolution;
+            cols = pictureBox1.Width / resolution;
+            field = new bool[cols, rows];
+
+            Random random = new Random();
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next((int)nudDensity.Value) == 0;
+                }
+            }
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+
+            timer1.Start();
+        }
+
+        private void StopGame()
+        {
+            if (!timer1.Enabled)
+                return;
+            else
+                timer1.Stop();
+
+            nudResolution.Enabled = true;
+            nudDensity.Enabled = true;
+        }
+
+        private void NextGeneration()
+        {
+            graphics.Clear(Color.Black);
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    if (field[x, y])
+                    {
+                        graphics.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution, resolution);
+                    }
+                }
+            }
+
+            Random random = new Random();
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next((int)nudDensity.Value) == 0;
+                }
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NextGeneration();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            StopGame();
+        }
+    }
+}
